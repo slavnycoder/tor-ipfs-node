a93c0875151ae68a7dd464600c4890b7276b7538
slavnycoder <efimov@atlas.ru>
2021-04-10 19:53:13 +0300

    initial

---
+++ up.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -euo pipefail
+cd `dirname $0`
+docker pull yassine/soxy-driver
+docker run -d -v '/var/run/docker.sock':'/var/run/docker.sock' -v '/run/docker/plugins':'/run/docker/plugins' --net host --name soxy-driver --privileged yassine/soxy-driver
+docker-compose up
