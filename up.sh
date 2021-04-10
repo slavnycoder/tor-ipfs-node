04940381833110ab5c12d7c1d739f4bc2e83c4a6
slavnycoder <efimov@atlas.ru>
2021-04-10 19:56:30 +0300

    add --rm parameter

--- up.sh
+++ up.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 cd `dirname $0`
 docker pull yassine/soxy-driver
-docker run -d -v '/var/run/docker.sock':'/var/run/docker.sock' -v '/run/docker/plugins':'/run/docker/plugins' --net host --name soxy-driver --privileged yassine/soxy-driver
+docker run --rm -d -v '/var/run/docker.sock':'/var/run/docker.sock' -v '/run/docker/plugins':'/run/docker/plugins' --net host --name soxy-driver --privileged yassine/soxy-driver
 docker-compose up
